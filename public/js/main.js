057db763e9886d9677d123e864e121f227239efe
newms34 <newms3450@gmail.com>
2016-04-27 19:17:00 -0400

    bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
 var mainApp = angular.module('mainApp', []);
 mainApp.controller('mainController', function($scope, $http) {
+	$scope.items;
     $scope.updateItems = function(){
     	console.log('refreshing items!')
     	//this function forces a refresh. It'll be our main way of getting updates until we get the setTimeout working!
     	$http.get('/rfr').then(function(resp){
     		console.log(resp);
+    		$scope.items = resp;
     	})
     }
+    $scope.updateItems();
 });
