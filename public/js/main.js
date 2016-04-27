de51aade80bf95c13caa41bb551ffb9f6f1298f5
newms34 <newms3450@gmail.com>
2016-04-27 19:22:29 -0400

    bug

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ mainApp.controller('mainController', function($scope, $http) {
     	//this function forces a refresh. It'll be our main way of getting updates until we get the setTimeout working!
     	$http.get('/rfr').then(function(resp){
     		console.log(resp);
-    		$scope.items = resp;
+    		$scope.items = resp.data;
     		$scope.$digest();
     	})
     }
